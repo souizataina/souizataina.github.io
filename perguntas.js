42fff10a919f8a01095186de58a48c99dede0bb6
souizataina <159554704+souizataina@users.noreply.github.com>
2024-10-09 11:24:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,67 @@
+criaCartao(
+    'matematica',
+    'qual o valor final da conta 4008% x 200 ?',
+    'resultado final 8.016'
+)
+
+criaCartao(
+    'literatura',
+    'Qual é seu livro favorito?',
+    'cem anos de solidão'
+)
+
+criaCartao(
+    'geografia',
+    'lugares do munto bons pra viajar?',
+    'japão, franca, eua '
+)
+
+criaCartao(
+    'filme',
+    'qual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
+criaCartao(
+    '',
+    'ual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
+criaCartao(
+    'matematica',
+    'qual o valor final da conta 4008% x 200 ?',
+    'resultado final 8.016'
+)
+
+criaCartao(
+    'literatura',
+    'Qual é seu livro favorito?',
+    'cem anos de solidão'
+)
+
+criaCartao(
+    'geografia',
+    'lugares do munto bons pra viajar?',
+    'japão, franca, eua '
+)
+
+criaCartao(
+    'filme',
+    'qual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
+criaCartao(
+    '',
+    'ual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
+
+criaCartao(
+    'filme',
+    'qual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
+criaCartao(
+    '',
+    'ual outro filme a atriz que fez crepusculo fez?',
+    ' a bela e a ferra livre atiton'
+)
